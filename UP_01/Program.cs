670ea1861526a80009b42a69877c4b398c1ac7d2
User <croaton4548@mail.ru>
2021-03-15 10:58:27 +0300

    first commit

---
+++ UP_01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UP_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
